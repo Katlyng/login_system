c1e89c43df24b70fdd7bbd85f75ec3900bc3b68b
Katlyng <katlyn.galvis@uptc.edu.co>
2025-02-23 21:16:16 -0500

    db variables added in .env

--- js/connection.js
+++ js/connection.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    database: "ls",
-    user: "root",
-    password: "root",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
+
 module.exports = { pool };
